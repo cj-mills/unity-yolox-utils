c0221151dc9a1e8b484a2cc39bbae92b8db11ee0
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-06 12:35:55 -0700

    Create PackageInstaller.cs

---
+++ Editor/PackageInstaller.cs
@@ -0,0 +1,100 @@
+using UnityEditor;
+using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CJM.YOLOXUtils
+{
+    [System.Serializable]
+    public class PackageData
+    {
+        public string packageName;
+        public string packageUrl;
+    }
+
+    [System.Serializable]
+    public class PackageList
+    {
+        public List<PackageData> packages;
+    }
+
+    public class PackageInstaller
+    {
+        private static AddRequest addRequest;
+        private static List<PackageData> packagesToInstall;
+        private static int currentPackageIndex;
+
+        [InitializeOnLoadMethod]
+        public static void InstallDependencies()
+        {
+            packagesToInstall = ReadPackageJson().packages;
+            currentPackageIndex = 0;
+
+            InstallNextPackage();
+        }
+
+        private static void InstallNextPackage()
+        {
+            if (currentPackageIndex < packagesToInstall.Count)
+            {
+                PackageData packageData = packagesToInstall[currentPackageIndex];
+                if (!IsPackageInstalled(packageData.packageName))
+                {
+                    addRequest = Client.Add(packageData.packageUrl);
+                    EditorApplication.update += PackageInstallationProgress;
+                }
+                else
+                {
+                    currentPackageIndex++;
+                    InstallNextPackage();
+                }
+            }
+        }
+
+        private static void PackageInstallationProgress()
+        {
+            if (addRequest.IsCompleted)
+            {
+                if (addRequest.Status == StatusCode.Success)
+                {
+                    UnityEngine.Debug.Log($"Successfully installed: {addRequest.Result.packageId}");
+                }
+                else if (addRequest.Status >= StatusCode.Failure)
+                {
+                    UnityEngine.Debug.LogError($"Failed to install package: {addRequest.Error.message}");
+                }
+
+                EditorApplication.update -= PackageInstallationProgress;
+                currentPackageIndex++;
+                InstallNextPackage();
+            }
+        }
+
+        private static bool IsPackageInstalled(string packageName)
+        {
+            var listRequest = Client.List(true, false);
+            while (!listRequest.IsCompleted) { }
+
+            if (listRequest.Status == StatusCode.Success)
+            {
+                return listRequest.Result.Any(package => package.name == packageName);
+            }
+            else
+            {
+                UnityEngine.Debug.LogError($"Failed to list packages: {listRequest.Error.message}");
+            }
+
+            return false;
+        }
+
+        private static PackageList ReadPackageJson()
+        {
+            string jsonPath = Path.Combine(Application.dataPath, "PackageInstaller", "packages.json");
+            string jsonString = File.ReadAllText(jsonPath);
+            return JsonUtility.FromJson<PackageList>(jsonString);
+        }
+    }
+}
