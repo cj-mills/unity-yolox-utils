c120714c1731514f775837a8619b4da7f2004d92
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-06 12:36:00 -0700

    Create AddCustomDefineSymbol.cs

---
+++ Editor/AddCustomDefineSymbol.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CJM.BBox2DToolkit
+{
+    public class DependencyDefineSymbolAdder
+    {
+        private const string CustomDefineSymbol = "CJM_YOLOX_UTILS";
+
+        [InitializeOnLoadMethod]
+        public static void AddCustomDefineSymbol()
+        {
+            var buildTargetGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+
+            if (!defines.Contains(CustomDefineSymbol))
+            {
+                defines += $";{CustomDefineSymbol}";
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+                Debug.Log($"Added custom define symbol '{CustomDefineSymbol}' to the project.");
+            }
+        }
+    }
+}
